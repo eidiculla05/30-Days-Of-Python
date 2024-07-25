cf568cfae72a25ea2f230a7b0fb517216ceed0c9
eidiculla05 <eli.idiculla5@gmail.com>
2024-07-25 13:09:27 -0400

    Update and rename variables.py to eidiculla05 - variables.py

---
+++ 02_Day_Variables_builtin_functions/eidiculla05 - variables.py
@@ -0,0 +1,17 @@
+
+# Day 2: 30 Days of Python Programming
+first_name = 'Eli'
+last_name = 'Idiculla'
+full_name = 'Eli Idiculla'
+country = 'United States of America'
+city = 'Philadelphia'
+age = 20
+year = '2024'
+is_married = 'No'
+is_true = 'Yes'
+is_light_on = 'Yes'
+
+first_name, last_name, full_name, age = 'Eli', 'Idiculla', 'Eli Idiculla', 20
+
+type(first_name)
+
